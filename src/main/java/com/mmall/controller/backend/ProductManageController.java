84fd2479aae22e9c4e65d5c133ec3a0eb22361da
SpringLych <2571568860@qq.com>
2018-11-21 15:13:17 +0800

    完成富文本文件上传

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -10,6 +10,7 @@ import com.mmall.service.IFileService;
 import com.mmall.service.IProductService;
 import com.mmall.service.IUserService;
 import com.mmall.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -159,6 +161,42 @@ public class ProductManageController {
             return ServerResponse.createBySuccess(fileMap);
         } else {
             return ServerResponse.createByErrorMsg("无操作权限");
+        }
+    }
+
+    /**
+     * 富文本上传
+     * simditor
+     */
+    @RequestMapping("richtext_img_upload.do")
+    @ResponseBody
+    public Map richtextImgUpload(HttpSession session, @RequestParam(value = "upload_file", required = false) MultipartFile file,
+                                 HttpServletRequest request, HttpServletResponse response) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        Map resultMap = Maps.newHashMap();
+        if (user == null) {
+            resultMap.put("success", false);
+            resultMap.put("msg", "请登录管理员");
+            return resultMap;
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file, path);
+            if (StringUtils.isBlank(targetFileName)) {
+                resultMap.put("success", false);
+                resultMap.put("msg", "上传失败");
+                return resultMap;
+            }
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+            resultMap.put("success", true);
+            resultMap.put("msg", "false");
+            resultMap.put("file_path", url);
+            response.addHeader("Access-Control-Allow-Headers", "X-File-Name");
+            return resultMap;
+        } else {
+            resultMap.put("success", false);
+            resultMap.put("msg", "无操作权限");
+            return resultMap;
         }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,5 +6,10 @@
     <input type="file" name="upload_file">
     <input type="submit" value="上传文件">
 </form>
+
+<form name="form1" action="/manage/product/richtext_img_upload.do.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file">
+    <input type="submit" value="上传文件">
+</form>
 </body>
 </html>
